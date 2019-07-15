af647a599f4c09c8b14f9670d85100274ac35aad
Michaela B <m.a.banczak@gmail.com>
2019-07-14 19:38:29 -0500

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-var PORT = 8080;
+var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
